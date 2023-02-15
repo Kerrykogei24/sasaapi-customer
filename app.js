ccf2cead7f5a6bde96322b641c7517ce9df59aa9
Kerrykogei24 <kerrykomar@gmail.com>
2023-02-15 10:26:05 +0300

    deployment

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+
+const rateLimiter = require('express-rate-limit')
+
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
@@ -23,12 +30,30 @@ const createCard = require('./routes/cards')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
-// app.get('/', (req, res) => {
-//     res.send('hello')
-// })
+
 
 app.use(express.json())
+app.set('trust proxy', 1)
+
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, //15 minutes
+        max: 100, //limit each ip to 100 request per windowMs
+    }))
+
+
+app.get('/', (req, res) => {
+
+    res.send('Sasakazi api')
+})
+
+
+
+
 
 
 app.use('/api/v1/me', authenticateUser, createCard, accountRouter, accountTopup, accountWithdraw);
